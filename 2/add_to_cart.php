bfb33bfb803b422dd48bae0f1cfacd13fe9a1192
Hai Luke <hailngch190599@fpt.edu.vn>
2024-03-05 23:55:52 +0700

    update db

--- 2/add_to_cart.php
+++ 2/add_to_cart.php
@@ -2,22 +2,34 @@
 session_start();
 include 'config.php'; 
 
+$user_id = session_id(); // Use the session ID as the user ID
+
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    if (isset($_SESSION['cart'][$id])) {
-        $_SESSION['cart'][$id]['quantity']++;
+    $product_id = $_GET['id'];
+    if (isset($_SESSION['cart'][$product_id])) {
+        $_SESSION['cart'][$product_id]['quantity']++;
+        $query = "UPDATE cart SET quantity = quantity + 1 WHERE user_id = '$user_id' AND product_id = $product_id";
+        if ($conn->query($query) === TRUE) {
+            echo 'success';
+        } else {
+            echo 'error updating cart: ' . $conn->error;
+        }
     } else {
-        $query = mysqli_query($conn, "SELECT * FROM products WHERE id = $id");
+        $query = mysqli_query($conn, "SELECT * FROM products WHERE id = $product_id");
         if ($query) {
             $row = mysqli_fetch_assoc($query);
             $_SESSION['cart'][$row['id']] = array("name" => $row['name'], "quantity" => 1, "price" => $row['price'], "image" => $row['image']);
-            echo 'success';
+            $query = "INSERT INTO cart (user_id, product_id, quantity) VALUES ('$user_id', $product_id, 1)";
+            if ($conn->query($query) === TRUE) {
+                echo 'success';
+            } else {
+                echo 'error inserting into cart: ' . $conn->error;
+            }
         } else {
-            echo 'error';
+            echo 'Product not found in products table';
         }
     }
 } else {
-    echo 'error';
+    echo 'Product ID not provided';
 }
 ?>
-    
--- 2/cart.php
+++ 2/cart.php
@@ -19,6 +19,7 @@ if (isset($_GET['action'])) {
         }
     }
 }
+
 ?>
 
 
